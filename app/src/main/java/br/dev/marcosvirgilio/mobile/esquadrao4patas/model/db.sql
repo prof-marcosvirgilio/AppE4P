580451ac8420ecd5a802dd105180e60f527bdf74
marcos.virgilio <marcos.virgilio@ifsc.edu.br>
2023-10-16 16:47:14 -0300

    2023-10-16 - Cadasto completo acessando webservice PHP

---
+++ app/src/main/java/br/dev/marcosvirgilio/mobile/esquadrao4patas/model/db.sql
@@ -0,0 +1,59 @@
+CREATE TABLE IF NOT EXISTS cor (
+  idcor INT UNSIGNED NOT NULL,
+  decor VARCHAR(150) NOT NULL,
+  PRIMARY KEY (idcor),
+  UNIQUE INDEX idcor_UNIQUE (idcor),
+  UNIQUE INDEX decor_UNIQUE (decor));
+
+
+CREATE TABLE IF NOT EXISTS porte (
+  idporte INT UNSIGNED NOT NULL,
+  deporte VARCHAR(150) NOT NULL,
+  PRIMARY KEY (idporte),
+  UNIQUE INDEX idporte_UNIQUE (idporte) ,
+  UNIQUE INDEX deporte_UNIQUE (deporte));
+
+
+CREATE TABLE IF NOT EXISTS animal (
+  idanimal INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  nmanimal VARCHAR(200) NULL,
+  nmtutor VARCHAR(200) NULL,
+  idcor INT UNSIGNED NOT NULL,
+  idporte INT UNSIGNED NOT NULL,
+  PRIMARY KEY (idanimal),
+  UNIQUE INDEX idanimal_UNIQUE (idanimal),
+  UNIQUE INDEX nmanimal_UNIQUE (nmanimal),
+  INDEX fk_animal_cor_idx (idcor),
+  INDEX fk_animal_porte1_idx (idporte));
+
+--cores
+  INSERT INTO cor (idcor,decor)
+  VALUES(0, 'Caramelo');
+
+  INSERT INTO cor (idcor,decor)
+  VALUES(1, 'Preto');
+
+  INSERT INTO cor (idcor,decor)
+  VALUES(2, 'Branco');
+
+  INSERT INTO cor (idcor,decor)
+  VALUES(3, 'Marrom');
+
+  --porte
+
+  INSERT INTO porte(idporte,deporte)
+  VALUES(0,'PP');
+
+  INSERT INTO porte(idporte,deporte)
+  VALUES(1,'P');
+
+  INSERT INTO porte(idporte,deporte)
+  VALUES(2,'M');
+
+  INSERT INTO porte(idporte,deporte)
+  VALUES(3,'G');
+
+  INSERT INTO porte(idporte,deporte)
+  VALUES(4,'GG');
+
+
--- app/src/main/java/br/dev/marcosvirgilio/mobile/esquadrao4patas/ui/animal/CadAnimalFragment.java
+++ app/src/main/java/br/dev/marcosvirgilio/mobile/esquadrao4patas/ui/animal/CadAnimalFragment.java
@@ -17,13 +17,25 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.snackbar.Snackbar;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import br.dev.marcosvirgilio.mobile.esquadrao4patas.R;
 import br.dev.marcosvirgilio.mobile.esquadrao4patas.model.Animal;
 
-public class CadAnimalFragment extends Fragment implements View.OnClickListener {
+public class CadAnimalFragment extends Fragment implements View.OnClickListener
+        , Response.ErrorListener,  Response.Listener{
     private View view = null;
     private CheckBox temTutor;
     private EditText nomeAnimal;
@@ -31,6 +43,10 @@ public class CadAnimalFragment extends Fragment implements View.OnClickListener 
     private Spinner porteAnimal;
     private Spinner corAnimal;
     private Button salvar;
+
+    //volley
+    private RequestQueue requestQueue;
+    private JsonObjectRequest jsonObjectReq;
 
 
     public CadAnimalFragment(){
@@ -50,7 +66,11 @@ public class CadAnimalFragment extends Fragment implements View.OnClickListener 
         this.corAnimal = (Spinner) view.findViewById(R.id.spCorPredominante);
         this.salvar = (Button) view.findViewById(R.id.btSalvar);
         this.salvar.setOnClickListener(this);
-
+        //
+        //instanciando a fila de requests - caso o objeto seja o view
+        this.requestQueue = Volley.newRequestQueue(view.getContext());
+//inicializando a fila de requests do SO
+        this.requestQueue.start();
         //
         return this.view;
     }
@@ -67,14 +87,56 @@ public class CadAnimalFragment extends Fragment implements View.OnClickListener 
                 animal.setCodTutor(this.codTutor.getSelectedItemPosition());
                 animal.setCodPorte(this.porteAnimal.getSelectedItemPosition());
                 animal.setCodCorPredominante(this.corAnimal.getSelectedItemPosition());
-                //mensagem de sucesso
-                Context context = view.getContext();
-                CharSequence text = "salvo com sucesso!";
-                int duration = Toast.LENGTH_SHORT;
-                Toast toast = Toast.makeText (context, text, duration);
-                toast.show();
+                //Chamar WebService
+                jsonObjectReq = new JsonObjectRequest(
+                        Request.Method.POST,
+                        "http://10.0.2.2/ws/cadanimal.php",
+                        animal.toJsonObject(), this, this);
+                requestQueue.add(jsonObjectReq);
                 break;
         }
+
+
+    }
+
+    @Override
+    public void onErrorResponse(VolleyError error) {
+        Snackbar mensagem = Snackbar.make(view,
+                "Ops! Houve um problema ao realizar o cadastro: " +
+                        error.toString(),Snackbar.LENGTH_LONG);
+        mensagem.show();
+
+    }
+
+    @Override
+    public void onResponse(Object response) {
+        try {
+            //instanciando objeto para manejar o JSON que recebemos
+            JSONObject jason = new JSONObject(response.toString());
+            //context e text são para a mensagem na tela o Toast
+            Context context = view.getContext();
+            //pegando mensagem que veio do json
+            CharSequence mensagem = jason.getString("message");
+            //duração da mensagem na tela
+            int duration = Toast.LENGTH_SHORT;
+            //verificando se salvou sem erro para limpar campos da tela
+            if (jason.getBoolean("success")){
+                //campos texto
+                this.nomeAnimal.setText("");
+                //selecionando primiro item dos spinners
+                this.codTutor.setSelection(0);
+                this.porteAnimal.setSelection(0);
+                this.corAnimal.setSelection(0);
+            }
+            //mostrando a mensagem que veio do JSON
+            Toast toast = Toast.makeText (context, mensagem, duration);
+            toast.show();
+
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+
+        //mensagem de sucesso
 
 
     }
